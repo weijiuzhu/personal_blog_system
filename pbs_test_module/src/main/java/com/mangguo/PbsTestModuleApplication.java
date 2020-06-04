468af4d40610969f28f8cb70a1d23f9b311b395f
weijiuzhu <12345678>
2020-06-04 17:44:18 +0800

    1，改造，添加多线程启动实时时间

--- pbs_test_module/src/main/java/com/mangguo/PbsTestModuleApplication.java
+++ pbs_test_module/src/main/java/com/mangguo/PbsTestModuleApplication.java
@@ -13,7 +13,7 @@ public class PbsTestModuleApplication implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("test模块起作用了。。。。。。");
-        log.error("这个输出了：${}", 1);
+        log.error("这个输出了：{}", 1);
         log.info("日志应该输出了的吧");
     }
 }
